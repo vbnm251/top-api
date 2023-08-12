5eccb306cb37b3e6e059cf989cdfbcb6ccc9d1ab
cha1l <iaanovi257@gmail.com>
2023-08-12 19:58:19 +0500

    finally actions

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
 
-  await app.listen(3001);
+  await app.listen(3000);
 }
 
 bootstrap();
